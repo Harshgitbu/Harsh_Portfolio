4cfda44bd2bf75e773a2f8dd100431f8bae2a955
Harshgitbu <harshbu@bu.edu>
2025-09-25 15:34:18 -0400

    adding hooks

---
+++ src/hooks/useFadeInOnScroll.js
@@ -0,0 +1,25 @@
+import { useEffect, useState, useRef } from 'react';
+
+const useFadeInOnScroll = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const domRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+          observer.disconnect();
+        }
+      });
+    });
+    if (domRef.current) {
+      observer.observe(domRef.current);
+    }
+    return () => observer.disconnect();
+  }, []);
+
+  return [domRef, isVisible];
+};
+
+export default useFadeInOnScroll;
